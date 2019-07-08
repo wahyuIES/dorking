2cfa264a34fdb63c6e61f4872bb243881ca63c94
wahyuIES <44322104+wahyuIES@users.noreply.github.com>
2019-07-08 12:58:08 +0700

    Update run-dork.py

--- run-dork.py
+++ run-dork.py
@@ -212,6 +212,15 @@ def cari(inurl , site , maxc):
     return finallist
 
 if __name__ == "__main__":
+	echo "
+_________________________________
+|<-- INDONESIAN ERROR SYSTEM -->|
+|__________MR.W4HYU_____________|
+    _______________
+   /  _/ ____/ ___/ coder :MR.W4HYU
+   / // __/  \__ \  team  :indonesian error system
+ _/ // /___ ___/ /  contac:wahyu.st021@gmail.com
+/___/_____//____/  
 	print (W+43*"-")
 
 	inurl = raw_input(B+"[</>]"+G+" Inurl    "+W+": ")
